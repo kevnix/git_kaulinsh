01af0e1227225d9dde2f71edeec85a7ef7607db9
Volters <kevinsvolters12@gmail.com>
2023-03-08 09:48:43 +0200

    Ieviesta iespēja ievadīt cik reizes mest kauliņu

--- src/git_kaulinsh/kaulish.java
+++ src/git_kaulinsh/kaulish.java
@@ -1,15 +1,23 @@
 package git_kaulinsh;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class kaulish {
 
 	public static void main(String[] args) {
-		int skaitlis;
+		int skaitlis, reizes;
 		Random rand = new Random();
-		skaitlis = rand.nextInt(6)+1;
-		System.out.println("Uzkrita skaitlis: "+skaitlis);
-
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cik reizes mest kaulu?");
+		reizes=scan.nextInt();
+		
+		for(int i=0;i<reizes;i++){
+			skaitlis = rand.nextInt(6)+1;
+			System.out.println("Uzkrita skaitlis: "+skaitlis);
+		}
+		
+		scan.close();
 	}
 
 }
