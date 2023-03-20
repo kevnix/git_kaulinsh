4a998ec42b0157099920f83416ae234f5d42504f
Volters <kevinsvolters12@gmail.com>
2023-03-20 16:03:22 +0200

    lokalaja vZaraa pieliku iespeju noteikt lielako ciparu

--- src/git_kaulinsh/kaulish.java
+++ src/git_kaulinsh/kaulish.java
@@ -6,12 +6,16 @@ import java.util.Scanner;
 public class kaulish {
 	
 	static void mestKaulinu(int skaits){
-		int skaitlis;
+		int skaitlis, lielakais=0;
 		Random rand = new Random();
 		for(int i=0; i<skaits; i++){
 			skaitlis = rand.nextInt(6)+1;
 			System.out.println("Uzkrita skaitlis: "+skaitlis);
+			
+			if(lielakais<skaitlis) lielakais=skaitlis;
 		}
+		
+		System.out.println("Lielâkais skaitlis: "+lielakais);
 	}
 
 	public static void main(String[] args) {
